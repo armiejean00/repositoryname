0519ec82897296da26764c9571cb9c0d6131edc5
kurtddbigtas@gmail.com <Kertsu>
2024-01-28 17:22:29 +0800

    prettify

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,26 +38,24 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $input = $request->all();
-        $this->validate($request,[
-            'email'=>'required|email',
-            'password'=>'required'
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
         ]);
-        if(auth()->attempt(array('email'=>$input['email'],'password'=>$input[
-            'password'
-        ]))){
-            if(auth()->user()->status==0){
-                return redirect()->route('welcome')->with('error','Your account is banned');
+        if (auth()->attempt(array('email' => $input['email'], 'password' => $input['password']))) {
+            if (auth()->user()->status == 0) {
+                return redirect()->route('welcome')->with('error', 'Your account is banned');
             }
-            if(auth()->user()->is_admin==1){
+            if (auth()->user()->is_admin == 1) {
                 return redirect()->route('admin.home');
-
-            }else{
+            } else {
                 return redirect()->route('home');
             }
-        }else{
-    return redirect()->route('login')->with('error','Input proper password/email');
-}
+        } else {
+            return redirect()->route('login')->with('error', 'Input proper password/email');
+        }
     }
 }
--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -11,92 +11,90 @@ class BookingController extends Controller
 {
 
 
-    
+
     //
     public function index()
-{
-    $bookings = Booking::all();
-    return view('bookings.index', compact('bookings'));
-}
-
-public function getSelfBookings () {
-    $self_bookings = Booking::all()->where('user_id', auth()->id());
-
-    return view('booking')->with('bookings', $self_bookings);
-}
-
-
-public function store(Request $request)
-{
-
-
-    $deskNumber = $request->input('desk_number');
-    $date = $request->input('date');
-
-    $isHotdeskReserved = Booking::where('desk_number', $deskNumber)->whereDate('date', $date)->exists();
-    
-
-    if ($isHotdeskReserved) {
-        return redirect()->back()->with('error', 'Desk is not available.');
-    } else {
-        $existingBooking = Booking::where('user_id', auth()->id())
-            ->whereDate('date', $date)
-            ->exists();
-        
-        if ($existingBooking) {
-            return redirect()->back()->with('error0', 'You already have a reservation for this date.');
-        }
-
-        $booking = Booking::create([
-            'date' => $date,
-            'desk_number' => $deskNumber,
-            'user_id' => auth()->id()
-        ]);
-        
-        return redirect()->back()->with('success', 'Booking created successfully.');
-
+    {
+        $bookings = Booking::all();
+        return view('bookings.index', compact('bookings'));
     }
 
-    // check if desk number is available
-    // $isAvailable = Desk::where('desk_number', $deskNumber)->where('isAvailable', '1')->exists();
-    
-    // if ($isAvailable) {
-    //     // check if the user has already booked a desk on the same day
-    //     $existingBooking = Booking::where('desk_number', $deskNumber)
-    //         ->whereDate('date', $date)
-    //         ->where('user_id', auth()->id()) // Add this condition to check the user ID
-    //         ->exists();
-        
-    //     if ($existingBooking) {
-    //         return redirect()->back()->with('error0', 'Desk is already booked on the specified date.');
-    //     }
-        
-    //     // get the current date
-    //     $currentDate = date('Y-m-d');
-        
-    //     // check if the booking date has arrived
-    //     if ($request->input('date') <= $currentDate) {
-    //         // update desk status to unavailable
-    //         Desk::where('desk_number', $deskNumber)->update(['isAvailable' => '0']);
-    //     } else {
-    //         // update desk status to available
-    //         Desk::where('desk_number', $deskNumber)->update(['isAvailable' => '1']);
-    //     }
-        
-    //     // create booking
-    //     $booking = Booking::create([
-    //         'date' => $request->input('date'),
-    //         'desk_number' => $deskNumber,
-    //         'user_id' => auth()->id(), // Add the user ID to the booking
-    //     ]);
-        
-    //     // additional logic or redirect the user as needed
-        
-    //     return redirect()->back()->with('success', 'Booking created successfully.');
-    // } else {
-    //     return redirect()->back()->with('error', 'Desk number is not available.');
-    // }
-}
+    public function getSelfBookings()
+    {
+        $self_bookings = Booking::all()->where('user_id', auth()->id());
+
+        return view('booking')->with('bookings', $self_bookings);
+    }
 
 
+    public function store(Request $request)
+    {
+
+
+        $deskNumber = $request->input('desk_number');
+        $date = $request->input('date');
+
+        $isHotdeskReserved = Booking::where('desk_number', $deskNumber)->whereDate('date', $date)->exists();
+
+
+        if ($isHotdeskReserved) {
+            return redirect()->back()->with('error', 'Desk is not available.');
+        } else {
+            $existingBooking = Booking::where('user_id', auth()->id())
+                ->whereDate('date', $date)
+                ->exists();
+
+            if ($existingBooking) {
+                return redirect()->back()->with('error0', 'You already have a reservation for this date.');
+            }
+
+            $booking = Booking::create([
+                'date' => $date,
+                'desk_number' => $deskNumber,
+                'user_id' => auth()->id()
+            ]);
+
+            return redirect()->back()->with('success', 'Booking created successfully.');
+        }
+
+        // check if desk number is available
+        // $isAvailable = Desk::where('desk_number', $deskNumber)->where('isAvailable', '1')->exists();
+
+        // if ($isAvailable) {
+        //     // check if the user has already booked a desk on the same day
+        //     $existingBooking = Booking::where('desk_number', $deskNumber)
+        //         ->whereDate('date', $date)
+        //         ->where('user_id', auth()->id()) // Add this condition to check the user ID
+        //         ->exists();
+
+        //     if ($existingBooking) {
+        //         return redirect()->back()->with('error0', 'Desk is already booked on the specified date.');
+        //     }
+
+        //     // get the current date
+        //     $currentDate = date('Y-m-d');
+
+        //     // check if the booking date has arrived
+        //     if ($request->input('date') <= $currentDate) {
+        //         // update desk status to unavailable
+        //         Desk::where('desk_number', $deskNumber)->update(['isAvailable' => '0']);
+        //     } else {
+        //         // update desk status to available
+        //         Desk::where('desk_number', $deskNumber)->update(['isAvailable' => '1']);
+        //     }
+
+        //     // create booking
+        //     $booking = Booking::create([
+        //         'date' => $request->input('date'),
+        //         'desk_number' => $deskNumber,
+        //         'user_id' => auth()->id(), // Add the user ID to the booking
+        //     ]);
+
+        //     // additional logic or redirect the user as needed
+
+        //     return redirect()->back()->with('success', 'Booking created successfully.');
+        // } else {
+        //     return redirect()->back()->with('error', 'Desk number is not available.');
+        // }
+    }
 }
